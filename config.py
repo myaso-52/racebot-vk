1cff94f147f2d5d9b8583d9937f4cd42c7da5463
myaso-52 <Lebedev.aleks12@gmail.com>
2026-07-24 20:09:04 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,29 @@
+# [file name]: config.py
+import os
+import json
+
+# Токен бота VK
+BOT_TOKEN = "vk1.a.4NLW0LW3cobhYjBFzUQ1uvIF8Zn93a7G9W--YJ-URTkk9tf9Qt7TCXYFGv1pQ-o17M_1oRUhJMEV53edLMcBKwIB9F3JIRJl-Vi0YXAAT26pOvv3_XY5Yc6wj6PQmt8p2BVheWDb4GKoIsjBkTT9pyVWWTK3qv0LZwZJv7FOFqczW5BAc7X9Hub2eaYgeWt9txSLeBYlbB-MiTG47JBKkQ"
+admins_ids = [819016396, 761815201]
+
+# Вебхук настройки
+CONFIRMATION_TOKEN = "f9691f4b"
+SECRET_KEY = "gonkaWow"
+GROUP_ID = 233724428
+
+# Настройки гонок
+RACE_DISTANCE = 1000
+GLOBAL_RACE_DISTANCE = 1500
+MAX_PLAYERS = 10
+MAX_PREMIUM_PLAYERS = 15
+MIN_PLAYERS = 2
+UPDATE_INTERVAL = 2
+GLOBAL_RACE_TIMEOUT = 900
+
+# Награды за уровни
+LEVEL_REWARD = 500
+
+
+# Google Sheets настройки
+GOOGLE_SHEET_ID = "1uQ1Vezfo85ypSl8vOBdzSCZN7Ueqp11PmSog7TVC3F8"  # Берешь из URL таблицы
+GOOGLE_CREDENTIALS = "google_credentials.json"  # Файл с ключами
