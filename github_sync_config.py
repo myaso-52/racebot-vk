e76af64329489b47c2ba7c44351e9d0868d43a6c
myaso-52 <Lebedev.aleks12@gmail.com>
2026-07-24 20:20:50 +0300

    Create github_sync_config.py

---
+++ github_sync_config.py
@@ -0,0 +1,6 @@
+# github_sync_config.py
+from config import GITHUB_API_KEY as gitkey
+GITHUB_REPO = "myaso-52/racebot-vk"
+FILES_TO_BACKUP = ["users.json", "chats.json", "admin.json", "payments.json"]
+BACKUP_INTERVAL = 10
+GITHUB_API_KEY = gitkey
